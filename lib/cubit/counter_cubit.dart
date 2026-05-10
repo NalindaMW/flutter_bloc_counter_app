2a74a36e7fe3b04e5a4ce3acc8f3a38d03208e37
Nalinda Wickramarathna <mastersilicon@gmail.com>
2026-05-10 18:06:44 +0530

    Added increment button functionality with cubit

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,10 +1,16 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<int> {
-  /// the constructor takes one argument and forwards it straight to Cubit’s constructor.
-  /// initial state is supplied by the caller each time and can be any integer
-  CounterCubit(super.initialState);
+  /// The constructor takes one argument and forwards it straight to Cubit’s constructor.
+  /// Initial state is supplied by the caller each time and can be any integer
+  // CounterCubit(super.initialState);
 
   /// This is a no-argument constructor that always passes 0 to Cubit.
-  // CounterCubit() : super(0);
+  CounterCubit() : super(0);
+
+  /// Increment state by 1
+  void increment() => emit(state + 1);
+
+  /// Decrement state by 1
+  void decrement() => emit(state - 1);
 }
